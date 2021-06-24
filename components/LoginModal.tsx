870927978003b72e2bb4d0ed27a6e58f41e14ac1
Chloe Sung <minji.sung.931009@gmail.com>
2021-06-24 23:06:59 +0900

    feature#9-FixedLogin

--- components/LoginModal.tsx
+++ components/LoginModal.tsx
@@ -1,23 +1,33 @@
-import React, { ChangeEvent, useCallback, useState } from "react";
-import { IUser } from "../interface/iUser";
+import React, { ChangeEvent, ReactElement, useCallback, useState } from "react";
+import Link from 'next/link';
+import Image from 'next/image';
+import mainLogo from '../images/unknown.png';
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../reducer";
-import { IUserActions } from "../interface/iUserActType";
-import styles from '../styles/modal.module.scss';
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import { SettingsInputAntenna } from "@material-ui/icons";
+import { userActionTypes } from "../interface/iUserActType";
+import styles from '../styles/loginModal.module.scss';
 
-const LoginModal = () => {
-  const router = useRouter()
+interface ChildProps {
+  handleModal: () => void;
+}
+
+const LoginModal: React.FC<ChildProps> = (props)=> {
   const dispatch = useDispatch();
   const { me, logInDone, logInLoading } = useSelector((state: RootState) => state.user);
   const [error, setError] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
   const [input, setInput] = useState({
     email: '',
     password: '',
   });
-  const { email, password } = input;
+
+  // const showModal = () => {
+  //   setIsOpen(true);
+  // }
+
+  // const closeModal = () => {
+  //   setIsOpen(false);
+  // }
 
   const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const {
@@ -29,25 +39,83 @@ const LoginModal = () => {
     })
   }
 
-  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+  const submitHandler = useCallback((e) => {
     e.preventDefault();
-    if (!input) {
-      setError('정확한 정보를 입력해주세요!');
-    }
-    return;
-  }
-  dispatch(login(email, password))
+    dispatch({
+      type: userActionTypes.LOG_IN_REQUEST,
+      data: {
+        email: input.email,
+        password: input.password,
+      },
+    })
+  }, [dispatch, input.email, input.password])
+
+  return (
+    <>
+        <div className={styles.modal} onClick={props.handleModal}>
+          <div onClick={e => {e.stopPropagation()}}>
+            <div className={styles.loginModal}>
+              <span className={styles.close} onClick={props.handleModal}>
+                &times;
+              </span>
+              <div className={styles.modalContents} onClick={props.handleModal}>
+                <Image
+                  // className={styles.signinIcon}
+                  src={mainLogo}
+                  alt="headerIMG"
+                  width={100}
+                  height={100}
+                />
+                <input
+                  name="email"
+                  className={styles.loginId}
+                  type="text"
+                  placeholder="아이디"
+                  onChange={inputChangeHandler}
+                />
+                <input
+                  name="password"
+                  className={styles.loginPw}
+                  type="password"
+                  placeholder="비밀번호"
+                  onChange={inputChangeHandler}
+                />
+                <div className={styles.loginMid}>
+                  <button className={styles.loginBtn} onClick={submitHandler}>
+                    {" "}
+                    로그인{" "}
+                  </button>
+                  <div className={styles.socialBox}>
+                    <div className={styles.kakao}>
+                      {/* <image
+                        className={styles.kakaoLogo}
+                        src="/Images/SignIn/kakao.png"
+                      /> */}
+                      <div className={styles.kakaoText}>카카오 계정으로 신규가입</div>
+                    </div>
+                    <div className={styles.google}>
+                      {/* <Image
+                        className={styles.googleLogo}
+                        src="/Images/SignIn/facebook.png"
+                      /> */}
+                      <div className={styles.googleText}>
+                        페이스북 계정으로 신규가입
+                      </div>
+                    </div>
+                  </div>
+                  <div className={styles.loginEnd}>
+                    <div className={styles.loginLine}>
+                      회원이 아니신가요? <Link href="/signup">회원가입</Link>
+                    </div>
+                    <div className={styles.noUse}>회원 가입 없이 체험</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </>
+  )
 }
-return (
-  <>
-    {!me && !logInDone ? (
-
-    )}
-  </>
-)
-}
-
-
 
 export default LoginModal;
-
